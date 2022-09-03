c0ae12087d8933e01ac15714a06a05483f0259f9
Yunuslala <saifjava2@gmail.com>
2022-09-03 22:31:21 +0530

    Varibles Assignment

---
+++ NearLatestQbasic/problem2.js
@@ -0,0 +1,7 @@
+var one = 1;
+var two = 2;
+var three =3;
+var four =4;
+var five=5;
+
+console.log((1*2)+(2*2)+(3*2)+(4*2)+(5*2));
---
+++ NearLatestQbasic/problem4.js
@@ -0,0 +1,3 @@
+var aman_purchase = 4000;
+let min_purchase = 4000;
+console.log(aman_purchase<=min_purchase);
